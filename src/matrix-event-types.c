3f7bf449896e2286e3857476a457dfc2b130fc33
Gergely Polonkai <gergely@polonkai.eu>
2016-03-08 17:41:34 +0100

    Add event handler for m.room.message.feedback

--- src/matrix-event-types.c
+++ src/matrix-event-types.c
@@ -197,6 +197,9 @@ matrix_event_types_ctor(void)
     matrix_event_register_type("m.room.avatar",
                                MATRIX_EVENT_TYPE_ROOM_AVATAR,
                                NULL);
+    matrix_event_register_type("m.room.message.feedback",
+                               MATRIX_EVENT_TYPE_ROOM_MESSAGE_FEEDBACK,
+                               NULL);
 }
 
 void
