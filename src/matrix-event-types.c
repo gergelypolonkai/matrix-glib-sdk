399105d9618464f486947468aca2e76ef3eb918f
Gergely Polonkai <gergely@polonkai.eu>
2016-03-09 10:04:44 +0100

    Add event handler for m.call.candidates

--- src/matrix-event-types.c
+++ src/matrix-event-types.c
@@ -212,6 +212,9 @@ matrix_event_types_ctor(void)
     matrix_event_register_type("m.call.invite",
                                MATRIX_EVENT_TYPE_CALL_INVITE,
                                NULL);
+    matrix_event_register_type("m.call.candidates",
+                               MATRIX_EVENT_TYPE_CALL_CANDIDATES,
+                               NULL);
 }
 
 void
