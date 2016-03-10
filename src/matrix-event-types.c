275a47b5e8d10f8c9c5c9dc6a601b4b2044b07b9
Gergely Polonkai <gergely@polonkai.eu>
2016-03-10 17:56:37 +0100

    Add message handler for m.file

--- src/matrix-event-types.c
+++ src/matrix-event-types.c
@@ -231,6 +231,9 @@ matrix_event_types_ctor(void)
     matrix_message_register_type("m.notice",
                                  MATRIX_MESSAGE_TYPE_NOTICE,
                                  NULL);
+    matrix_message_register_type("m.file",
+                                 MATRIX_MESSAGE_TYPE_FILE,
+                                 NULL);
 }
 
 void
